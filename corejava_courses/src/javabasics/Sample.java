138a849750650b3b0c017bde7641d271bb6a31ef
kalai135 <kalaiarumugham135@gmail.com>
2024-03-05 15:45:48 +0530

    This is my first project

---
+++ corejava_courses/src/javabasics/Sample.java
@@ -0,0 +1,17 @@
+package javabasics;
+import java.util.*;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter a and b");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c= a+b;
+		System.out.println("Addition of a = "+a+" and b = "+b+" is : "+c);
+		
+	}
+
+}
