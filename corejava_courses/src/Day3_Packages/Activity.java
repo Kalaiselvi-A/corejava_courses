995b26f4cc4e50dd2a83bfc51d2c74f5ce0d95e7
kalai135 <kalaiarumugham135@gmail.com>
2024-03-11 15:49:39 +0530

    Scanner

---
+++ corejava_courses/src/Day3_Packages/Activity.java
@@ -0,0 +1,14 @@
+package Day3_Packages;
+import java.util.*;
+
+public class Activity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String name = sc.nextLine();
+		System.out.println("Hello Coder,"+name);
+	}
+
+}
